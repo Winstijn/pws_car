6b132c3cf1fc2727fa48217f16a8c626735143b2
winstijn <winstijn@hotmail.nl>
2019-12-09 16:36:44 +0000

    Added track sample to drive circles

---
+++ vehicle_control/cirkels.js
@@ -0,0 +1,30 @@
+//
+//  PWS AI
+//  Testing VehicleControls in this file!
+//
+//  Created by Winstijn Smit
+//  Copyright © 2019 Kaziaat B.V. All rights reserved.
+//
+
+const VehicleControls = require('./vehicle_controls.js')
+
+let controls = new VehicleControls( { powerLimit: 30, onReady: circles } )
+
+async function circles(){
+    console.log('Driving circles!')
+    controls.resetCar();
+    await wait(500);
+
+    controls.setSteer(Math.PI);
+    await wait(500);
+
+    controls.setAccelarator(1)
+    await wait(10000);
+
+    controls.resetCar();
+}
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
